e548492054d1517c4dac67d6410dc0dde44f984a
vkrajsa <vkrajsa@gmail.com>
2021-10-22 21:43:34 +0200

    Start project

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'bootstrap';
+import './assets/styles/app.scss'
+import i18n from './i18n'
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   router,
   store,
   render: h => h(App)
