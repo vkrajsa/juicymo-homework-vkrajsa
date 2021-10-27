eaf8573a6472ae2cb6923314b204490f98235943
vkrajsa <vkrajsa@gmail.com>
2021-10-27 11:14:52 +0200

    Add AboutPage

--- src/router/index.js
+++ src/router/index.js
@@ -28,7 +28,8 @@ const routes = [
   {
     path: '/about',
     name: 'AboutPage',
-    component: AboutPage
+    component: AboutPage,
+
   },
 
   // {
--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,5 +1,32 @@
 <template>
-  <main>
-    <h1>This is an about page</h1>
+  <main id="aboutPage">
+    <div class="container px-4 py-5">
+      <div class="row g-5">
+        <div class="col-lg-6">
+          <h1 class="my-5">Hi, I am Vojta</h1>
+          <p class="lead">
+            Poslední 4 roky jsem kódoval weby pro vlastní potřebu a pro zábavu.
+            Rozhodl jsem se změnit kariérní směr a z vod marketingu se přesunout
+            do front-end developmentu. Hledám tým, který vezme pod svá křídla
+            začínajícího frontendistu.
+          </p>
+        </div>
+
+        <div class="row mt-5">
+          <div class="d-grid col-lg-4">
+            <a
+              href="https://www.vojtechkrajsa.cz"
+              class="btn btn-primary"
+              target="blank"
+              >my portfolio</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<script>
+export default {};
+</script>
